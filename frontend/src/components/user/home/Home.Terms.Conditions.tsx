b16474f8087382971f1b4096d9f13fddd957f917
Martin John M <johnmmartin74@gmail.com>
2025-01-18 10:01:57 +0100

    slight responsiveness changes

--- frontend/src/components/user/home/Home.Terms.Conditions.tsx
+++ frontend/src/components/user/home/Home.Terms.Conditions.tsx
@@ -3,11 +3,11 @@
 
 export const HomeTermsCondition = () => {
     return <div className="w-full text-slate-500 max-w-7xl container mx-auto p-4">
-        <div className="grid grid-cols-2">
-            <div>
+        <div className="lg:grid lg:grid-cols-2">
+            <div className=" col-span-1">
                 <p>This is a sample website by <span className="font-bold">Martin John M</span> © 2024 – All Rights Reserved</p>
             </div>
-            <div className="justify-center items-center flex">
+            <div className="flex col-span-1">
                 <div className="flex gap-4">
                     <div className="hover:underline cursor-pointer">
                         <p>Sitemap</p>
